5806244ab36fe0a0262867deefe0722ffb1dcadc
MaheryJeremie <mramahay2103@gmail.com>
2025-05-06 00:15:43 +0300

    Ajout de fonctionnalite
    Creation de supplier quotation a partir de request quotation

--- src/main/java/org/example/erpspring/controller/quotation/QuotationController.java
+++ src/main/java/org/example/erpspring/controller/quotation/QuotationController.java
@@ -4,7 +4,9 @@ import org.example.erpspring.service.quotation.QuotationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -31,7 +33,44 @@ public class QuotationController {
         model.addAttribute("editable",editable);
         return "quotation/itemList";
     }
+    @GetMapping("/rfqs/{supplierId}")
+    public String getRfqsByCustomer(@PathVariable String supplierId,Model model){
+        model.addAttribute("quotations", quotationService.getRFQsBySupplier(supplierId));
+        model.addAttribute("supplierName", supplierId);
+        return "quotation/rfqs";
+    }
+    @GetMapping("/rfqs/item")
+    public String getRfqsItems(@RequestParam String rfq,@RequestParam String supplierName,Model model){
+        List<Map<String,Object>> items = quotationService.getRfqItems(rfq);
+        model.addAttribute("items",items);
+        model.addAttribute("rfq",rfq);
+        model.addAttribute("supplierName",supplierName);
+        return "quotation/rfqItemList";
+    }
+    @PostMapping("/create")
+    public String createSupplierQuotation(
+            @RequestParam String rfq,
+            @RequestParam String supplierName,
+            @RequestParam Map<String, String> allParams,
+            RedirectAttributes redirectAttributes) {
 
+        Map<String, Double> itemRates = new HashMap<>();
+        for (String paramName : allParams.keySet()) {
+            if (paramName.startsWith("rate_")) {
+                String itemCode = paramName.substring(5);
+                double rate = Double.valueOf(allParams.get(paramName));
+                itemRates.put(itemCode, rate);
+            }
+        }
+        try {
+            String sqNumber = quotationService.createSupplierQuotation(rfq, supplierName, itemRates);
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("error", "Erreur lors de la création: " + e.getMessage());
+            return "redirect:/quotations/rfqs/item?rfq="+rfq+"&supplierName="+supplierName;
+        }
+
+        return "redirect:/quotations/"+supplierName;
+    }
 
 }
 
--- src/main/java/org/example/erpspring/service/quotation/QuotationService.java
+++ src/main/java/org/example/erpspring/service/quotation/QuotationService.java
@@ -2,14 +2,22 @@ package org.example.erpspring.service.quotation;
 
 import org.example.erpspring.service.FrappeService;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class QuotationService {
-    private static final String QUOTATION_ENDPOINT = "/resource/Supplier Quotation";
+    private static final String REQUEST_QUOTATION_ENDPOINT = "/resource/Request for Quotation";
+    private static final String SUPPLIER_QUOTATION_ENDPOINT = "/resource/Supplier Quotation";
+    private static final String SUBMIT_ENDPOINT = "/method/frappe.client.submit";
 
     private final FrappeService apiService;
 
@@ -19,7 +27,7 @@ public class QuotationService {
 
     public List<Map<String, Object>> getSupplierQuotations(String supplierId) {
         var response = apiService.get(
-                QUOTATION_ENDPOINT,
+                SUPPLIER_QUOTATION_ENDPOINT,
                 Map.of(
                         "fields", "[\"name\", \"status\", \"valid_till\", \"grand_total\"]",
                         "filters", "[[\"supplier\", \"=\", \"" + supplierId + "\"]]",
@@ -31,10 +39,86 @@ public class QuotationService {
 
     public List<Map<String, Object>> getQuotationItems(String name) {
         var response = apiService.get(
-                QUOTATION_ENDPOINT+"/"+name,null
+                SUPPLIER_QUOTATION_ENDPOINT+"/"+name,null
         );
-        System.out.println( response.getBody());
         Map<String, Object> data =  (Map<String, Object>)  response.getBody().get("data");
         return (List<Map<String, Object>>) data.get("items");
+    }
+
+    public List<Map<String, Object>> getAllRequestQuotations() {
+        var response = apiService.get(
+                REQUEST_QUOTATION_ENDPOINT,
+                Map.of(
+                        "fields", "[\"name\",\"transaction_date\",\"suppliers.supplier\"]"
+                )
+        );
+        return (List<Map<String, Object>>) response.getBody().get("data");
+    }
+    public List<Map<String, Object>> getRFQsBySupplier(String supplierName) {
+        List<Map<String, Object>> allRfqs = getAllRequestQuotations();
+        return allRfqs.stream()
+                .filter(rfq -> supplierName.equals(rfq.get("supplier")))
+                .collect(Collectors.toList());
+    }
+    public List<Map<String, Object>> getRfqItems(String name) {
+        var response = apiService.get(
+                REQUEST_QUOTATION_ENDPOINT+"/"+name,null
+        );
+        Map<String, Object> data =  (Map<String, Object>)  response.getBody().get("data");
+        return (List<Map<String, Object>>) data.get("items");
+    }
+    public String createSupplierQuotation(String rfq, String supplierName, Map<String, Double> itemRates) {
+        List<Map<String, Object>> rfqItems = getRfqItems(rfq);
+
+        List<Map<String, Object>> sqItems = new ArrayList<>();
+        for (Map<String, Object> item : rfqItems) {
+            String itemCode = (String) item.get("item_code");
+            Map<String, Object> sqItem = new HashMap<>();
+            sqItem.put("item_code", itemCode);
+            sqItem.put("qty", item.get("qty"));
+            sqItem.put("rate", itemRates.get(itemCode));
+            sqItem.put("schedule_date", LocalDate.now().toString());
+            sqItems.add(sqItem);
+        }
+
+        Map<String, Object> sqData = new HashMap<>();
+        sqData.put("supplier", supplierName);
+        sqData.put("items", sqItems);
+        sqData.put("transaction_date", LocalDate.now().toString());
+        sqData.put("valid_till", LocalDate.now().toString());
+        sqData.put("company", "Hehe");
+        sqData.put("request_for_quotation", rfq);
+
+        ResponseEntity<Map> response=apiService.send(
+                SUPPLIER_QUOTATION_ENDPOINT,
+                sqData,
+                HttpMethod.POST
+        );
+        if (response.getBody() == null || !response.getBody().containsKey("data")) {
+            throw new RuntimeException("Failed to create payment entry: No response data");
+        }
+        String sqEntryName = ((Map<String, String>) response.getBody().get("data")).get("name");
+//        ResponseEntity<Map> getResponse = apiService.get(
+//                SUPPLIER_QUOTATION_ENDPOINT + "/" + sqEntryName,
+//                null
+//        );
+//
+//        if (getResponse.getBody() == null || !getResponse.getBody().containsKey("data")) {
+//            throw new RuntimeException("Échec de récupération du Payment Entry : " + sqEntryName);
+//        }
+//
+//        Map<String, Object> sqEntry = (Map<String, Object>) getResponse.getBody().get("data");
+//        Map<String, Object> submitData = new HashMap<>();
+//        submitData.put("doc", sqEntry);
+//        ResponseEntity<Map> submitResponse = apiService.send(
+//                SUBMIT_ENDPOINT,
+//                submitData,
+//                HttpMethod.POST
+//        );
+//
+//        if (submitResponse.getStatusCode() != HttpStatus.OK) {
+//            throw new RuntimeException("Échec de soumission du Payment Entry : " + submitResponse.getBody());
+//        }
+        return sqEntryName;
     }
 }
